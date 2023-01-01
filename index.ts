429a102b5ddb854a736e36ff733b7006b6cfea8e
Gregor Gabric <gregor.gabric@preskok.si>
2023-01-01 19:13:40 +0100

    testing seomthing 2

--- index.ts
+++ index.ts
@@ -4,12 +4,19 @@ for (const entry of currentDir) {
   if (entry.isDirectory) {
     const fileName = entry.name;
     const renamedFile = fileName + "1";
+    if (fileName === ".git") {
+      continue;
+    }
 
     Deno.run({
       cmd: ["git", "mv", fileName, renamedFile],
+      stdout: "piped",
+      stderr: "piped",
     });
     Deno.run({
       cmd: ["git", "mv", renamedFile, fileName.toLowerCase()],
+      stdout: "piped",
+      stderr: "piped",
     });
   }
 }

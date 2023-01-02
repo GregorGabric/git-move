ad652cbba5414caea9db7830729fc45d8dd0c8bb
Gregor Gabric <gregor.gabric@preskok.si>
2023-01-02 14:30:26 +0100

    init

--- index.ts
+++ index.ts
@@ -3,20 +3,33 @@ const currentDir = Deno.readDirSync(".");
 for (const entry of currentDir) {
   if (entry.isDirectory) {
     const fileName = entry.name;
+
+    const lowerCaseFileName = [...fileName]
+      .map((letter, index) => {
+        if (index === 0) {
+          return letter.toLowerCase();
+        }
+        if (letter === letter.toUpperCase()) {
+          return "-" + letter.toLowerCase();
+        }
+        return letter.toLowerCase();
+      })
+      .join("");
+
     const renamedFile = fileName + "1";
+
     if (fileName === ".git") {
       continue;
     }
 
-    Deno.run({
+    const rename = Deno.run({
       cmd: ["git", "mv", fileName, renamedFile],
-      stdout: "piped",
-      stderr: "piped",
     });
-    Deno.run({
-      cmd: ["git", "mv", renamedFile, fileName.toLowerCase()],
-      stdout: "piped",
-      stderr: "piped",
+    await rename.status();
+
+    const correctName = Deno.run({
+      cmd: ["git", "mv", renamedFile, lowerCaseFileName],
     });
+    await correctName.status();
   }
 }
